f15c977ec10151ba38d27158ccd7f83a47c28e88
Daniel Butum <danibutum@gmail.com>
2015-10-21 21:43:22 +0300

    Return content type 'application/json' from JSON view.

--- userreport/views/opengl_json.py
+++ userreport/views/opengl_json.py
@@ -8,8 +8,9 @@ from django.shortcuts import render_to_response
 
 
 def report_opengl_json(request):
+    content_type = 'application/json'
     if not ENABLE_JSON:
-        return HttpResponse(json.dumps({'message': 'JSON Disabled'}), content_type='text/plain')
+        return HttpResponse(json.dumps({'message': 'JSON Disabled'}), content_type=content_type)
 
     devices = {}
     reports = GraphicsDevice.objects.all()
@@ -20,6 +21,8 @@ def report_opengl_json(request):
         device = (report.vendor, report.renderer, report.os, report.driver)
         devices.setdefault((HashableDict(limits), exts), set()).add(device)
 
+    # TODO a lot of keys and values are repeated, maybe once the JSON is usable we can cache it every 24h
+    # and make the JSON output much smaller
     sorted_devices = devices.items()  # sorted(devices.items(), key=devices.items().get)
 
     data = []
@@ -28,7 +31,7 @@ def report_opengl_json(request):
         data.append({'devices': devices, 'limits': limits, 'extensions': sorted(exts)})
 
     json_string = json.dumps(data, indent=1, sort_keys=True)
-    return HttpResponse(json_string, content_type='text/plain')
+    return HttpResponse(json_string, content_type=content_type)
 
 
 def report_opengl_json_format(request):
