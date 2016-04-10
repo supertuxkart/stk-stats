5497c132d80e423c1ef629ae0bec68e2e442044a
Edward Betts <edward@4angle.com>
2016-04-10 08:01:05 +0100

    use % formatter (#8)
    Use % formatter

--- maint_graphics.py
+++ maint_graphics.py
@@ -13,7 +13,7 @@ from userreport import maint
 start_time = time.time()
 remove_time, get_time, save_time = maint.refresh_data()
 total_time = time.time() - start_time
-print("--- Remove Time: {:>5.2f} seconds, {:>5.2f}% ---".format(remove_time, remove_time / total_time * 100))
-print("--- Get    Time: {:>5.2f} seconds, {:>5.2f}% ---".format(get_time, get_time / total_time * 100))
-print("--- Save   Time: {:>5.2f} seconds, {:>5.2f}% ---".format(save_time, save_time / total_time * 100))
+print("--- Remove Time: {:>5.2f} seconds, {:>5.2%} ---".format(remove_time, remove_time / total_time))
+print("--- Get    Time: {:>5.2f} seconds, {:>5.2%} ---".format(get_time, get_time / total_time))
+print("--- Save   Time: {:>5.2f} seconds, {:>5.2%} ---".format(save_time, save_time / total_time))
 print("--- Total  Time: {:>5.2f} seconds ---".format(total_time))
